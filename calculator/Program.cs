4e3c3f43ef5afe9e07c6cbe84620e5563069cee7
carlos cadena <carloscadena@live.com>
2018-10-08 12:09:29 -0700

    Calculator working without error handling

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,7 +6,42 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter some numbers to calculate (Acceptable operators: +, -, x, /) in this format: 4 x 5");
+            string input = Console.ReadLine();
+            string[] calculate = input.Split(' ');
+            if (calculate[1] == "+")
+            {
+                Add(calculate[0], calculate[2]);
+            }
+            else if (calculate[1] == "-")
+            {
+                Subtract(calculate[0], calculate[2]);
+            }
+            else if (calculate[1] == "x")
+            {
+                Multiply(calculate[0], calculate[2]);
+            }
+            else
+            {
+                Divide(calculate[0], calculate[2]);
+            }
+
+        }
+        static void Add(string a, string b)
+        {
+            Console.WriteLine($"The sum of {a} and {b} is {int.Parse(a) + int.Parse(b)}");
+        }
+        static void Subtract(string a, string b)
+        {
+            Console.WriteLine($"The difference between {a} and {b} is {int.Parse(a) - int.Parse(b)}");
+        }
+        static void Multiply(string a, string b)
+        {
+            Console.WriteLine($"The product of {a} and {b} is {int.Parse(a) * int.Parse(b)}");
+        }
+        static void Divide(string a, string b)
+        {
+            Console.WriteLine($"The quotient of {a} and {b} is {int.Parse(a) / int.Parse(b)}");
         }
     }
 }
